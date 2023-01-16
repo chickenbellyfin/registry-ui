2db7ff4c3e289bd3bc349a8423901a5d81046270
chickenbellyfin <chickenbellyfinn@gmail.com>
2023-01-15 18:07:19 -0800

    wip

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import re
 
 import jinja2
-from sanic import Sanic, response
+from sanic import Sanic, response, Request
 
 from src.api import ApiCredentials, DockerApiV2, UnauthorizedApiError
 from src.data_fetch import fetch_image_list, fetch_repositories, fetch_tags
@@ -34,39 +34,44 @@ def create_app(url: str, registry: DockerApiV2, theme: str, enable_login) -> San
 
   app.static('/static', 'static')
 
-  def auth(func):
-    if not enable_login:
-      return func
-
-    async def wrapped(request, *args, **kwargs):
-      try:
-        username = request.cookies.get('registry_username')
-        password = request.cookies.get('registry_password')
-        creds = ApiCredentials(username, password)
-        return await func(request, *args, **kwargs, creds=creds)
-      except UnauthorizedApiError as e:
-        res = response.redirect('/login')
-        del res.cookies['registry_username']
-        del res.cookies['registry_password']
+  if enable_login:
+    @app.on_request
+    async def extract_auth(request: Request):
+      username = request.cookies.get('registry_username')
+      password = request.cookies.get('registry_password')    
+      print(f"extract_auth {username} {password}")
+      request.ctx.creds = ApiCredentials(username, password)
+    
+    @app.exception(UnauthorizedApiError)
+    async def handle_unauthorized(request, exception):
+      res = response.redirect('/login')
+      del res.cookies['registry_username']
+      del res.cookies['registry_password']
       return res
-    return wrapped
 
-  @app.get('/login')
-  async def login(request):
-    response = await render('login.html', context=context)
-    return response
+    @app.get('/login')
+    async def login(request):
+      response = await render('login.html', context=context)
+      return response
 
-  @app.post('/submit_login')
-  async def submit_login(request):
-    res = response.redirect('/')
-    res.cookies['registry_username'] = request.form.get('username')
-    res.cookies['registry_password'] = request.form.get('password')
-    return res
+    @app.post('/submit_login')
+    async def submit_login(request):
+      res = response.redirect('/')
+      res.cookies['registry_username'] = request.form.get('username')
+      res.cookies['registry_password'] = request.form.get('password')
+      return res
+  else:
+    @app.on_request
+    async def empty_auth(request: Request):
+      request.ctx.creds = None
+    
+    @app.exception(UnauthorizedApiError)
+    async def handle_unauthorized(request, exception):
+      return await render('unauthorized.html', context=context)
 
   @app.get('/')
-  @auth
-  async def list_repositories(request, creds=None):
-    repositories = await fetch_repositories(registry, creds=creds)
+  async def list_repositories(request: Request):
+    repositories = await fetch_repositories(registry, creds=request.ctx.creds)
     return await render(
       'repositories.html',
       context=context,
@@ -74,10 +79,9 @@ def create_app(url: str, registry: DockerApiV2, theme: str, enable_login) -> San
     )
 
   @app.get('/repo')
-  @auth
-  async def list_tags(request, creds=None):
+  async def list_tags(request: Request):
     repo = request.args.get('repo')
-    tags = await fetch_tags(registry, repo, creds=creds)
+    tags = await fetch_tags(registry, repo, creds=request.ctx.creds)
     return await render('tags.html',
       context=context,
       tags=tags,
@@ -85,11 +89,10 @@ def create_app(url: str, registry: DockerApiV2, theme: str, enable_login) -> San
     )
 
   @app.get('/image')
-  @auth
-  async def tag_history(request, creds=None):
+  async def tag_history(request: Request):
     repo = request.args.get('repo')
     tag = request.args.get('tag')
-    images =  await fetch_image_list(registry, repo, tag, creds=creds) # registry.history(repo, tag)
+    images =  await fetch_image_list(registry, repo, tag, creds=request.ctx.creds) # registry.history(repo, tag)
     return await render('history.html',
       context=context,
       images=images,
